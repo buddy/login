6ee463a032081f0fc8b2538638a109895954a01d
Cyprian Zdebski <cyprian.zdebski@buddy.works>
2025-06-04 16:24:25 +0200

    change tsup to esbuild and improve build step

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
-import core from '@actions/core';
-import github from '@actions/github';
+import {setFailed, getInput, setOutput} from '@actions/core';
+import {context} from '@actions/github';
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
+    const nameToGreet = getInput('who-to-greet');
     console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    setOutput("time", time);
+    const payload = JSON.stringify(context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
 } catch (error) {
-    core.setFailed(error.message);
+    setFailed(error.message);
 }
