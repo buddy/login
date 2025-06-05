9de86ed93caef4a0b48c637f953290f9fc72c084
Cyprian Zdebski <cyprian.zdebski@buddy.works>
2025-06-05 11:11:55 +0200

    add mock get buddy token fn

--- src/login.ts
+++ src/login.ts
@@ -2,12 +2,20 @@ import { getInputs } from '@/utils/action/getInputs'
 import { getIDToken } from '@actions/core'
 import { IOutput } from '@/types/output'
 import { ISecret } from '@/types/env'
+import { IInput } from '@/types/input'
 
 export async function login(): Promise<IOutput> {
   const inputs = getInputs()
   const jwt = await getIDToken(inputs.audience) as ISecret
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts')
-  const data = await response.json()
+  const data = await mockGetBuddyToken(inputs.region, inputs.providerId, jwt)
   return { jwt, data }
 }
 
+
+async function mockGetBuddyToken(region: IInput['region'], providerId: IInput['providerId'], jwt: ISecret): Promise<string> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`mocked-token-for-${region}-${providerId}-${jwt}`)
+    }, 1000)
+  })
+}
